bb7edcdfa21c408eca19b20571180fcdcef801fe
Gautham Yaramasa <gauthamyaramasa@infynitelabs.com>
2026-04-27 18:33:19 +0530

    8-push

--- kindeSrc/components/animated-sphere.tsx
+++ kindeSrc/components/animated-sphere.tsx
@@ -6,7 +6,7 @@ export const AnimatedSphere: React.FC = () => {
   return (
     <div className="sphere-wrap" aria-hidden="true">
       <video autoPlay={true} loop={true} muted={true} playsInline={true} preload="auto">
-        <source src="/sphere.webm" type="video/webm" />
+        <source src="https://cdn.jsdelivr.net/gh/Gauthamyaramasa/auth-code@master/public/sphere.webm" type="video/webm" />
       </video>
     </div>
   );
